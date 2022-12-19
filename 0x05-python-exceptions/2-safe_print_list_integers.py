46c2431747a91ede4be2491af2e2c898d2a5ae8b
sharon5555 <sharon.doris54@gmail.com>
2022-12-19 04:35:40 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,7 +10,7 @@ def safe_print_list_integers(my_list=[], x=0):
         except ValueError:
             pass
         else:
-             count += 1
+            count += 1
 
-             print()
-             return (count)
+     print()
+     return (count)
