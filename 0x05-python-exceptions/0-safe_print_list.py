be6ad971e9965265bce016e75429b50829556698
sharon5555 <sharon.doris54@gmail.com>
2022-12-19 05:47:01 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,10 +5,12 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-             except:
-                 break
-             else:
-                 count += 1
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        else:
+            count += 1
 
     print()
     return (count)
